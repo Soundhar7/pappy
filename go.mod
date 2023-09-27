baabac3ee66c38b7da1617acb9f73297667cc87e
Soundhar7 <soundhar991999@gmail.com>
2023-09-27 06:47:29 +0530

    sec

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Soundhar7/pappy
+
+go 1.21.1
---
+++ pappy.go
@@ -0,0 +1,13 @@
+package pappy
+
+func Bark() string{
+
+return "woof! woof!"
+
+
+}
+
+func Barks() string{
+
+	return "woof! woof! woof!"
+}
